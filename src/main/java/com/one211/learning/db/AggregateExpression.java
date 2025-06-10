5b79bb0f2f98e5490e571f5b369ba02147acfd35
Ekant2008 <ekantyadav2008@gmail.com>
2025-06-11 00:18:57 +0530

    max and sum

--- src/main/java/com/one211/learning/db/AggregateExpression.java
+++ src/main/java/com/one211/learning/db/AggregateExpression.java
@@ -27,4 +27,47 @@ public interface AggregateExpression extends Expression {
             return state;
         }
     }
+    public final class Max implements AggregateExpression{
+        private final Expression expression;
+        Object state = null;
+        public Max(Expression expression) {
+            this.expression = expression;
+        }
+        @Override
+        public Object finalValue() {
+            return state;
+        }
+
+        @Override
+        public Object apply(Row row)  {
+            Object current = expression.apply(row);
+            if ( state == null) {
+                state = current;
+            } else {
+                state = Math.max((Integer) current, (Integer) state);
+            }
+            return state;
+        }
+    }
+    public  final class Sum implements AggregateExpression{
+        public final Expression expression;
+        Double sum =0.0;
+        public Sum(Expression expression){
+            this.expression=expression;
+        }
+        @Override
+        public Object finalValue() {
+            return sum;
+        }
+
+        @Override
+        public Object apply(Row row) {
+            Object current = expression.apply(row);
+            if (current instanceof Number) {
+                sum += ((Number) current).doubleValue();
+            }
+            return sum;
+        }
+    }
+
 }
